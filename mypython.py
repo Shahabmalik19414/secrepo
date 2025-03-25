de3c098e6b7b2d2d9644cb44a8a2a8673a251ff6
Shahab Malik <shahabmalik19414@gmail.com>
2025-03-25 22:27:07 +0500

    Create mypython.py

---
+++ mypython.py
@@ -0,0 +1,15 @@
+import time
+
+def task1():
+    print("Task 1 started...")
+    time.sleep(3)  # Simulating a 3-second task
+    print("Task 1 completed!")
+
+def task2():
+    print("Task 2 started...")
+    time.sleep(2)  # Simulating a 2-second task
+    print("Task 2 completed!")
+
+task1()
+task2()
+print("All tasks completed!")
